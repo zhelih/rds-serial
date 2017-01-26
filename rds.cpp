d60256175f5f92fe0cc1146716baf6a494ecbec7
Eugene Lykhovyd <lykhovyd@gmail.com>
2017-01-25 18:01:09 -0600

    allocate RDS memory in the beginning (square)

--- rds.cpp
+++ rds.cpp
@@ -34,20 +34,13 @@ void print_lb_atomic(int signal)
   exit(0);
 }
 
-uint find_max(vector<uint>& c, vector<uint>& p, const uint* mu, verifier *v, graph* g, vector<uint>& res, void* aux)
+uint find_max(vector<vector <uint> >& c, vector<uint>& p, const uint* mu, verifier *v, graph* g, vector<uint>& res, void* aux, int level)
 {
 //  printf("debug: running find_max with c.size = %lu, p.size = %lu\n", c.size(), p.size());
-  if(c.size() == 0)
+  if(c[level].size() == 0)
   {
-//    return max(p.size(), lb);
     if(p.size() > lb)
     {
-/*      printf("Updating new lb solution to:\n");
-      for(auto it = p.begin(); it != p.end(); ++it)
-      {
-        printf("%u ", *it);
-      }
-      printf("\n");*/
       res = p; //copy
       return p.size();
     }
@@ -55,39 +48,35 @@ uint find_max(vector<uint>& c, vector<uint>& p, const uint* mu, verifier *v, gra
       return lb;
   }
 
-  while(c.size() > 0)
+  while(c[level].size() > 0)
   {
-    if(c.size() + p.size() < lb) // Prune 1
+    if(c[level].size() + p.size() < lb) // Prune 1
       return lb;
-    //TODO replace find_min here to constant storing
-    //auto it = min_element(c.begin(), c.end()); // FIXME O(n) time
-/*    uint ind = it-c.begin();
-    printf("ind = %u\n", ind);*/
-    uint i = c[c.size()-1];
-    if(mu[i] + p.size() < lb)
+    uint i = c[level][c[level].size()-1];
+    if(mu[i] + p.size() < lb) // Prune 2
       return lb;
-    c.pop_back();
+    c[level].pop_back();
 //    NB: exploit that we adding only 1 vertex to p
 //    thus verifier can prepare some info using prev calculations
-    v->prepare_aux(g, p, i, c, aux);
+    v->prepare_aux(g, p, i, c[level], aux);
     p.push_back(i);
-    vector<uint> c_new;
-    for(auto it2 = c.begin(); it2 != c.end(); ++it2)
+    c[level+1].resize(0);
+    for(uint it2 = 0; it2 < c[level].size(); ++it2)
     {
-      if(*it2 != i && v->check(g, p, *it2, aux))
+      if(c[level][it2] != i && v->check(g, p, c[level][it2], aux))
       {
-        c_new.push_back(*it2);
+        c[level+1].push_back(c[level][it2]);
       }
     }
 /*    if(c_new.size() == 0) {
       if(p.size() > lb)
         lb = p.size();
     } else */
-    lb = find_max(c_new, p, mu, v, g, res, aux);
+    lb = find_max(c, p, mu, v, g, res, aux, level+1);
     lb_a = lb;
     p.pop_back();
 //    if(aux != 0)
-    v->undo_aux(g, p, i, c, aux);
+    v->undo_aux(g, p, i, c[level], aux);
   }
   return lb;
 }
@@ -107,21 +96,27 @@ uint rds(verifier* v, graph* g, vector<uint>& res, uint time_lim)
     // form candidate set
     // take vertices from v \in {i+1, n} for which pair (i,v) satisfies \Pi
     // first iteration c is empty, that must set bound to 1
-    vector<uint> c;
+    vector<vector<uint> > c;
+    c.resize(g->nr_nodes);
+    for(uint j = 0; j < g->nr_nodes; ++j)
+    {
+      c[j].reserve(g->nr_nodes);
+      c[j].resize(0);
+    }
     for(uint j = i+1; j < n; ++j)
     {
       if(v->check_pair(g, i, j))
       {
         // add to C
-        c.push_back(j);
+        c[0].push_back(j);
       }
     }
-    reverse(c.begin(), c.end()); // for efficient deletion of min element
+    reverse(c[0].begin(), c[0].end()); // for efficient deletion of min element
     vector<uint> p;
     p.push_back(i);
-    void* aux = v->init_aux(g, i, c);
-    printf("i = %u, c.size = %lu, ", i, c.size());
-    mu[i] = find_max(c, p, mu, v, g, res, aux);
+    void* aux = v->init_aux(g, i, c[0]);
+    printf("i = %u, c.size = %lu, ", i, c[0].size());
+    mu[i] = find_max(c, p, mu, v, g, res, aux, 0);
     printf("mu[%d] = %d\n", i, mu[i]);
     v->free_aux(aux);
     if(time_lim > 0 && (clock()-start)/CLOCKS_PER_SEC >= time_lim)
