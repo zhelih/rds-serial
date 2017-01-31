99c7d13c118f12b70570c9b5453148f2d49bc55a
Eugene Lykhovyd <lykhovyd@gmail.com>
2017-01-31 17:31:44 -0600

    add weight support

--- rds.cpp
+++ rds.cpp
@@ -38,17 +38,16 @@ void print_lb_atomic(int signal)
 static uint iter = 0;
 static bool should_exit = false;
 
-uint find_max(vector<vector <uint> >& c, vector<uint>& p, const uint* mu, verifier *v, graph* g, vector<uint>& res, int level, const chrono::time_point<chrono::steady_clock> start, const uint time_lim)
+uint find_max(vector<vector <uint> >& c, vector<uint>& weight_c, vector<uint>& p, uint& weight_p, const uint* mu, verifier *v, graph* g, vector<uint>& res, int level, const chrono::time_point<chrono::steady_clock> start, const uint time_lim)
 {
-//  printf("debug: running find_max with c.size = %lu, p.size = %lu\n", c.size(), p.size());
   if(should_exit)
     return lb;
   if(c[level].size() == 0)
   {
-    if(p.size() > lb)
+    if(weight_p > lb)
     {
       res = p; //copy
-      return p.size();
+      return weight_p;
     }
     else
       return lb;
@@ -67,32 +66,28 @@ uint find_max(vector<vector <uint> >& c, vector<uint>& p, const uint* mu, verifi
       }
     }
 
-    if(c[level].size() + p.size() <= lb) // Prune 1
+    if(weight_c[level] + weight_p <= lb) // Prune 1
       return lb;
     uint i = c[level][c[level].size()-1];
-    if(mu[i] + p.size() <= lb) // Prune 2
+    if(mu[i] + weight_p <= lb) // Prune 2
       return lb;
-    c[level].pop_back();
+    c[level].pop_back(); weight_c[level] -= g->weight(i);
 //    NB: exploit that we adding only 1 vertex to p
 //    thus verifier can prepare some info using prev calculations
     v->prepare_aux(g, p, i, c[level]);
-    p.push_back(i);
-    c[level+1].resize(0);
+    p.push_back(i); weight_p += g->weight(i);
+    c[level+1].resize(0); weight_c[level+1] = 0;
     for(uint it2 = 0; it2 < c[level].size(); ++it2)
     {
       if(c[level][it2] != i && v->check(g, p, c[level][it2]))
       {
         c[level+1].push_back(c[level][it2]);
+        weight_c[level+1] += g->weight(c[level][it2]);
       }
     }
-/*    if(c_new.size() == 0) {
-      if(p.size() > lb)
-        lb = p.size();
-    } else */
-    lb = find_max(c, p, mu, v, g, res, level+1, start, time_lim);
+    lb = find_max(c, weight_c, p, weight_p, mu, v, g, res, level+1, start, time_lim);
     lb_a = lb;
-    p.pop_back();
-//    if(aux != 0)
+    p.pop_back(); weight_p -= g->weight(i);
     v->undo_aux(g, p, i, c[level]);
   }
   return lb;
@@ -114,12 +109,13 @@ uint rds(verifier* v, graph* g, vector<uint>& res, uint time_lim)
     // form candidate set
     // take vertices from v \in {i+1, n} for which pair (i,v) satisfies \Pi
     // first iteration c is empty, that must set bound to 1
-    vector<vector<uint> > c;
+    vector<vector<uint> > c; vector<uint> weight_c(g->nr_nodes);
     c.resize(g->nr_nodes);
     for(uint j = 0; j < g->nr_nodes; ++j)
     {
       c[j].reserve(g->nr_nodes);
       c[j].resize(0);
+      weight_c[j] = 0;
     }
     for(uint j = i+1; j < n; ++j)
     {
@@ -127,14 +123,15 @@ uint rds(verifier* v, graph* g, vector<uint>& res, uint time_lim)
       {
         // add to C
         c[0].push_back(j);
+        weight_c[0] += g->weight(j);
       }
     }
     reverse(c[0].begin(), c[0].end()); // for efficient deletion of min element
-    vector<uint> p;
-    p.push_back(i);
+    vector<uint> p; uint weight_p = 0;
+    p.push_back(i); weight_p += g->weight(i);
     v->init_aux(g, i, c[0]);
     printf("i = %u, c.size = %lu, ", i, c[0].size());
-    mu[i] = find_max(c, p, mu, v, g, res, 0, start, time_lim);
+    mu[i] = find_max(c, weight_c, p, weight_p, mu, v, g, res, 0, start, time_lim);
     printf("mu[%d] = %d\n", i, mu[i]);
     v->free_aux();
     if(time_lim > 0)
