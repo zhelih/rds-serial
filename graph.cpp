670bc5ceb014f3da4867e37824869c3939e13d7e
Mykyta Makovenko <tsumiman@gmail.com>
2018-09-18 22:56:11 -0500

    Fix a bug

--- graph.cpp
+++ graph.cpp
@@ -139,15 +139,16 @@ std::vector<uint> graph::reverse_order(const std::vector<uint>& order) const
 void graph::reorder_custom(const vector<uint>& order)
 {
   std::vector<uint> order_reverse = this->reverse_order(order);
-
-  bool adj_new[nr_nodes][nr_nodes];
+  bool* adj_new = (bool*)malloc(sizeof(bool)*nr_nodes*nr_nodes);
   for (uint v = 0; v < nr_nodes; ++v)
     for (uint u = 0; u < nr_nodes; ++u)
-      adj_new[order_reverse[v]][order_reverse[u]] = this->adj[v][u];
+      adj_new[order_reverse[v]*nr_nodes + order_reverse[u]] = this->adj[v][u];
 
   for (uint v = 0; v < nr_nodes; ++v)
     for (uint u = 0; u < nr_nodes; ++u)
-      this->adj[v][u] = adj_new[v][u];
+      this->adj[v][u] = adj_new[v*nr_nodes + u];
+
+  free(adj_new);
 
   std::vector<uint> weights_new(nr_nodes);
   for (uint v = 0; v < nr_nodes; ++v)
