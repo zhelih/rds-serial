913820f50d5e245b569b992edd64118f0fc41867
Mykyta Makovenko <tsumiman@gmail.com>
2018-10-25 17:09:32 -0500

    Add reordering to adjacency-list graph

--- graph/graph_adjacency.cpp
+++ graph/graph_adjacency.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <algorithm>
 #include <set>
+#include <iterator>
 #include <cstdlib>
 
 void graph_adjacency::separate_vertex(uint v) {
@@ -13,27 +14,16 @@ void graph_adjacency::separate_vertex(uint v) {
 
 void graph_adjacency::reorder_custom(const std::vector<uint>& order)
 {
-/*
   std::vector<uint> order_reverse = this->reverse_order(order);
-  bool* adj_new = (bool*)malloc(sizeof(bool)*nr_nodes*nr_nodes);
-  for (uint v = 0; v < nr_nodes; ++v)
-    for (uint u = 0; u < nr_nodes; ++u)
-      adj_new[order_reverse[v]*nr_nodes + order_reverse[u]] = this->adj[v][u];
+  for (uint v = 0; v < this->nr_nodes; ++v) {
+    std::unordered_set<uint> new_adj;
+    std::transform(this->adj[v].begin(), this->adj[v].end(), std::inserter(new_adj, new_adj.end()), [&](const uint &v){return order_reverse[v];});
+    this->adj[v] = new_adj;
+  }
 
-  for (uint v = 0; v < nr_nodes; ++v)
-    for (uint u = 0; u < nr_nodes; ++u)
-      this->adj[v][u] = adj_new[v*nr_nodes + u];
-
-  free(adj_new);
-
-  std::vector<uint> weights_new(nr_nodes);
-  for (uint v = 0; v < nr_nodes; ++v)
-    weights_new[order_reverse[v]] = this->weights[v];
-  this->weights = weights_new;
-
-  std::vector<uint> new_order(nr_nodes);
-  for (uint i = 0; i < nr_nodes; ++i)
-    new_order[i] = this->current_order[order[i]];
-  this->current_order = new_order;
-*/
+  std::vector<std::unordered_set<uint>> new_adj(this->nr_nodes);
+  for (uint v = 0; v < this->nr_nodes; ++v) {
+    new_adj[order_reverse[v]] = adj[v];
+  }
+  adj = new_adj;
 }
